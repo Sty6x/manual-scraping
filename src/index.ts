c645104596262effd094dcb8bac22c88d64e0aac
francis <franzdiaz460@gmail.com>
2023-09-01 19:04:29 +0400

    ref: creating an xlsx file with extracted emails

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,17 @@ import fs from "fs/promises";
 import { JSDOM } from "jsdom";
 import parseArgs from "minimist";
 import EventEmitter from "events";
+import ExcelJS from "exceljs";
+import { workerData } from "worker_threads";
 const events = new EventEmitter();
 const argv = parseArgs(process.argv.splice(2));
-
+const workbook = new ExcelJS.Workbook();
+const worksheet =
+  workbook.getWorksheet("Email list") === undefined
+    ? workbook.addWorksheet("Email list")
+    : workbook.getWorksheet("Email list");
 events.addListener("start", getFile);
+events.emit("start", argv);
 events.addListener("write", writeEmails);
 
 async function getFile({
@@ -24,15 +31,25 @@ async function getFile({
 }
 
 async function writeEmails(emails: HTMLElement[]) {
-  const extractEmails = emails.map((email) => `\n${email.title}`);
   try {
-    const write = await fs.writeFile("output.txt", extractEmails);
-    console.log("success");
-    console.log(write);
+    worksheet.columns = [{ header: "", key: "emails" }];
+    const filteredEmails = emails.filter((email) => email.title !== "");
+    const emailCol = worksheet.getColumn(1);
+    emailCol.width = 50;
+    const newRows = filteredEmails.map((email) => ({
+      emails: email.title,
+    }));
+    // if (worksheet.rowCount !== 0) {
+    //   worksheet.eachRow((row, i) => {
+    //     console.log(row.values);
+    //   });
+    //   return;
+    // }
+    newRows.forEach((row) => worksheet.addRow(row));
+    await workbook.xlsx.writeFile("output.xlsx");
+    console.log(newRows);
   } catch (err) {
     console.log("yikes");
     console.log(err);
   }
 }
-
-events.emit("start", argv);
